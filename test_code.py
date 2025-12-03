fcb888a41ea75735442e74e9c5b7b05a594ae280
Danielthomaspaulsuresh <danielthomaspaulsuresh@gmail.com>
2025-12-04 05:10:27 +0930

    python programing

---
+++ test_code.py
@@ -0,0 +1,24 @@
+name = "KARTHI"
+watch = 400
+print(name ,"baught it for", watch)
+
+
+watch1 = watch2 = watch3 = 200
+
+print(watch1)
+
+name = "danny"
+print(name[-4:-2])
+print(len(name))
+
+print(name.strip())
+print(name.upper())
+print(name.lower())
+print(name.replace('y','i'))
+print(name.split('a'))
+print("dant" in name)
+a1 ="danny"
+a2 = ' thomas'
+
+print(a1 + a2)
+print(1>10)

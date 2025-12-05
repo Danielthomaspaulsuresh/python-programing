0a06660a788346ca049c7576c2c607405989f5ad
Danielthomaspaulsuresh <danielthomaspaulsuresh@gmail.com>
2025-12-06 04:29:05 +0930

    loop

---
+++ loop.py
@@ -0,0 +1,22 @@
+
+# forloop
+"""
+name = "danny"
+for letters in name:
+    print (letters)
+
+"""
+fruits = ["apple","banna","cherry","orange"]
+
+for letter in fruits:
+    print(letter)
+
+name = "danny"
+
+for i in name:
+
+    if i == "d":
+        print("d : is present")
+    else:
+        print("its not present")
+    print(i)
